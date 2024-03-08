7b417a0a75f003129e00416a0ab0bce1b4bcaa23
Abdullah-Ahmed1 <abdullah.ahmed10001@gmail.com>
2024-03-08 20:41:43 +0500

    shop header added

---
+++ src/app/shop/page.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Shop = () => {
+  return (
+    <div>
+      <div className="bg-red-500 w-full h-80 relative">
+        <div className="absolute top-0 left-0 flex flex-col justify-center items-center w-full h-80 gap-5">
+          <p className="text-3xl font-bold">Shop</p>
+          <p>{"Home > Shop"}</p>
+        </div>
+      </div>
+      <div className="bg-cream">das</div>
+    </div>
+  );
+};
+
+export default Shop;
