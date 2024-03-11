47e5bcb8060d4f2ca857283cf5b9503e3a216ab2
Abdullah-Ahmed1 <abdullah.ahmed10001@gmail.com>
2024-03-11 18:00:12 +0500

    code clean

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import LoginForm from "./form";
-import shopify from "@/utils/shopify";
 import { gql } from "graphql-request";
 import DataContainer from "./dataContainer";
 
@@ -14,9 +13,7 @@ const LoginPage = () => {
       },
     };
     const query = gql`
-      mutation customerAccessTokenCreate(
-        $input: CustomerAccessTokenCreateInput!
-      ) {
+      mutation customerAccessTokenCreate($input: CustomerAccessTokenCreateInput!) {
         customerAccessTokenCreate(input: $input) {
           customerUserErrors {
             code
@@ -30,21 +27,6 @@ const LoginPage = () => {
         }
       }
     `;
-
-    // try {
-    //   const result = await shopify(query, input);
-    //   const error = result?.customerAccessTokenCreate?.customerUserErrors;
-    //   const token = result?.customerAccessTokenCreate?.customerAccessToken;
-    //   if (error) console.log("error is:", error);
-    //   if (token) console.log("token is:", token);
-    //   console.log("result", result);
-    // } catch (error) {
-    //   console.error("Error:", error);
-    //   return {
-    //     status: 500,
-    //     error: "Error receiving data",
-    //   };
-    // }
   };
 
   return (
