58f735e629257705e86eb7b02ff49ba223312d10
Abdullah-Ahmed1 <abdullah.ahmed10001@gmail.com>
2024-03-12 12:41:31 +0500

    types error resolved

--- src/utils/shopify.ts
+++ src/utils/shopify.ts
@@ -1,9 +1,8 @@
 import { GraphQLClient } from "graphql-request";
 
 const shopify = async (query: any, variables: any) => {
-  const headers: { [key: string]: string | undefined } = {
-    "X-Shopify-Storefront-Access-Token": process.env.SHOPIFY_STOREFRONT_ACCESS_TOKEN,
-  };
+  const headers = new Headers();
+  headers.set("X-Shopify-Storefront-Access-Token", process.env.SHOPIFY_STOREFRONT_ACCESS_TOKEN || "");
   const graphQLClient = new GraphQLClient("https://bessersol.myshopify.com/api/2024-01/graphql.json", {
     headers,
   });
