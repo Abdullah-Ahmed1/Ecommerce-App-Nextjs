940dfbd23ce06ed11cad19ac73a21e9f1cadf49a
Abdullah Ahmed <abdullah.ahmed10001@gmail.com>
2024-06-19 00:55:08 +0500

    fix: cart modal

--- src/app/checkout/@checkoutModal/(..)checkout/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const CheckoutModal = () => {
-  return <div className="h-screen w-screen bg-red-500">Page</div>;
-};
-
-export default CheckoutModal;
---
+++ src/app/shop/@modal/(..)cart/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CheckoutModal = () => {
+  return (
+    <div className="fixed top-0 z-[10000] h-screen w-screen bg-red-500">
+      Page
+    </div>
+  );
+};
+
+export default CheckoutModal;
